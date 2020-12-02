9db5a33d6aabc03fb5ef050376b270b20a3c800f
Tham <zarub2k@gmail.com>
2020-12-02 18:43:28 +0530

    blog page view work started

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,19 +1,50 @@
 <template>
-  <div>
-    <h1>Blog Posts</h1>
-    <ul>
-      <li v-for="article of articles" :key="article.slug">
-        <NuxtLink :to="{ name: 'blog-slug', params: { slug: article.slug } }">
-          <img :src="article.img" />
-          <div>
-            <h2>{{ article.title }}</h2>
-            <!-- <p>by {{ article.author.name }}</p> -->
-            <p class="text-gray-600">{{ article.description }}</p>
-          </div>
-        </NuxtLink>
-      </li>
-    </ul>
-  </div>
+  <section class="p-3 px-10">
+    <div class="grid grid grid-cols-3 gap-4">
+      <div class="col-span-2">
+        <figure class="my-5">
+          <img src="~/assets/images/flutter.png"
+              alt="Elephant at sunset">
+          <figcaption>
+            <div>{{ recent.title }}</div>
+            <div>{{ recent.description }} </div>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="my-5">
+        Top Picks
+      </div>
+    </div>
+
+    <div class="grid grid grid-cols-3 gap-10 border">
+      <div class="border">
+        One
+      </div>
+      <div class="border">
+        Two
+      </div>
+      <div class="border">
+        Three
+      </div>
+      <div class="border">Four</div>
+    </div>
+
+    <div>
+      <h1>Blog Posts</h1>
+      <ul>
+        <li v-for="article of articles" :key="article.slug">
+          <NuxtLink :to="{ name: 'blog-slug', params: { slug: article.slug } }">
+            <img :src="article.img" />
+            <div>
+              <h2>{{ article.title }}</h2>
+              <!-- <p>by {{ article.author.name }}</p> -->
+              <p class="text-gray-600">{{ article.description }}</p>
+            </div>
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -24,8 +55,10 @@
         .sortBy('createdAt', 'asc')
         .fetch()
 
+        const recent = articles[0]
+
       return {
-        articles
+        articles, recent
       }
     }
   }
