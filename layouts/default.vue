398d3b952f4603806e79ea8d74253d7052f880e0
Tham <zarub2k@gmail.com>
2020-12-02 18:20:03 +0530

    Header font color is changed

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,22 +2,22 @@
   <main>
     <div class="grid grid-cols-4 gap-4 p-3 border">
       <div>
-        <NuxtLink to="/">Logo</NuxtLink>
+        <NuxtLink to="/" class="font-sans text-xl antialiased text-indigo-700">Logo</NuxtLink>
       </div>
       <div class="col-span-2">
         <div class="flex">
           <div class="flex-1">
-            <NuxtLink to="/service">Services</NuxtLink>
+            <NuxtLink to="/service" class="text-indigo-500">Services</NuxtLink>
           </div>
           <div class="flex-1">
-            <NuxtLink to="/blog">Blog</NuxtLink>
+            <NuxtLink to="/blog" class="text-indigo-500">Blog</NuxtLink>
           </div>
           <div class="flex-1">
-            <NuxtLink to="/contact">Contact me</NuxtLink>
+            <NuxtLink to="/contact" class="text-indigo-500">Contact me</NuxtLink>
           </div>
         </div>
       </div>
-      <div>Final</div>
+      <div class="text-indigo-500">Final</div>
     </div>
     
     <Nuxt class="" />
