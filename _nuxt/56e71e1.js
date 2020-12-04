32e2eee40ec5f70478d023cced2058aa4c36e024
Tham <zarub2k@gmail.com>
2020-12-04 21:27:43 +0530

    Updates

--- _nuxt/56e71e1.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{239:function(e,n,t){"use strict";t.r(n);var c=t(26),component=Object(c.a)({},(function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("Service page")])}),[],!1,null,null,null);n.default=component.exports}}]);
