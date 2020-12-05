df77b0ff29a92896ae264674d76155049c500888
Tham <zarub2k@gmail.com>
2020-12-05 22:57:03 +0530

    Author work continuous

--- components/Author.vue
+++ components/Author.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-    <figure>
-      <img src="~/assets/images/tham.png" alt="">
-      <figcaption>
-        <div>
-          <h4>{{ author.name }}</h4>
+    <figure class="bg-gray-100 rounded-xl p-8">
+      <img class="w-32 h-32 rounded-full mx-auto" src="~/assets/images/tham.png" alt="Author Tham">
+      <figcaption class="font-medium">
+        <div class="text-cyan-600">
+          {{ author.name }}
+        </div>
+        <div class="text-gray-500">
+          {{ author.bio }}
         </div>
       </figcaption>
     </figure>
