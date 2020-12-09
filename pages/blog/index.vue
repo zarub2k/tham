dae9e88f75829508832c15a80aafec3cde520972
Tham <zarub2k@gmail.com>
2020-12-09 19:49:57 +0530

    subscribe button added

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -39,7 +39,11 @@
     <div class="flex justify-center p-2 mt-5">
       <div class="text-center w-full p-10 border inline bg-gradient-to-r from-teal-400 to-blue-500">
         <h2 class="mb-5 text-3xl text-white">Technology and growth hacks for your inbox</h2>
-        <input type="text" class="w-1/2 h-10 p-2 border-gray-300 focus:border-blue-200 text-gray-600">
+        <p>
+          <input type="text" class="w-1/2 h-10 p-2 border-gray-300 focus:border-blue-200 text-gray-600">
+          <button class="bg-teal-900 p-2 text-white">Subscribe</button>
+        </p>
+        
       </div>
     </div>
   </section>
