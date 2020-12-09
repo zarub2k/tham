f451da9df6d0d0d818a28474ccbd367df5e967e2
Tham <zarub2k@gmail.com>
2020-12-09 19:56:29 +0530

    Subscribe button is created

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -40,8 +40,8 @@
       <div class="text-center w-full p-10 border inline bg-gradient-to-r from-teal-400 to-blue-500">
         <h2 class="mb-5 text-3xl text-white">Technology and growth hacks for your inbox</h2>
         <p>
-          <input type="text" class="w-1/2 h-10 p-2 border-gray-300 focus:border-blue-200 text-gray-600">
-          <button class="bg-teal-900 p-2 text-white">Subscribe</button>
+          <input type="text" v-model="email" class="w-1/2 h-10 p-2 border-gray-300 focus:border-blue-200 text-gray-600">
+          <button @click="onSubscribe" class="bg-teal-900 p-2 text-white">Subscribe</button>
         </p>
         
       </div>
@@ -51,6 +51,17 @@
 
 <script>
   export default {
+    data () {
+      return {
+        email: ''
+      }
+    },
+    methods: {
+      onSubscribe: function() {
+        console.log('Email subscribed successfully!')
+        this.email = ''
+      }
+    },
     // data () {
     //   return {
     //     page: 0
